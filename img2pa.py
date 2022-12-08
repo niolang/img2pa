6d31e8984fbfae10413826fb5150a4297e4207c9
N LNG <nlng@BULLSEYE-debian-live-builder>
2022-12-08 21:59:21 +0100

    first iteration, script is working, need to pass file name, and treshold(s) as argument of script

--- img2pa.py
+++ img2pa.py
@@ -1,2 +1,11 @@
-from PIL import image
+from PIL import Image
+import os
 
+dir = os.getcwd()
+img = Image.open(dir+'/bg_construct.png')
+
+for i in range(0, 251, 25): #for multiple iteration
+    thresh = i
+    fn = lambda x : 255 if x > thresh else 0
+    r = img.convert('L').point(fn, mode='1')
+    r.save(dir + '/bg_const_bw_' + str(i) + '.png')
