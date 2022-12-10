f4fc2a62b23069c09aa7c8c648badf7eb7a331f4
Niolang <nlangroudi@gmail.com>
2022-12-10 11:35:40 +0100

    programm update with creation of function

--- img2pa.py
+++ img2pa.py
@@ -5,24 +5,29 @@ import sys
 #git image file you want to use
 dir = os.getcwd()
 img = Image.open(dir+'/'+ sys.argv[1])
+fullname = dir + '/' + sys.argv[1].split(".")[0]
+
+def bwimg(threshold, full_path):
+    fn = lambda x : 255 if x > threshold else 0
+    r = img.convert('L').point(fn, mode='1')
+    r.save(full_path + '_' + str(threshold) + '.png')
 
 #Help
 if "-h" in str(sys.argv):
     print("You asked for help with -h, for threshold use -t <value>, for iteration -i <value>")
-    exit()
-elif "-t" in str(sys.argv):
-    thresh = int(sys.argv[3])
-    fn = lambda x : 255 if x > thresh else 0
-    r = img.convert('L').point(fn, mode='1')
-    r.save(dir + '/bg_const_bw_' + str(thresh) + '.png')
+    print(fullname)
     exit()
 
+#Create 1 b&w image with 1 threshold
+elif "-t" in str(sys.argv):
+    bwimg(int(sys.argv[3]), fullname)
+    exit()
+
+#Create multiple images with iterations as thresholds
 elif "-i" in str(sys.argv):
     for i in range(0, 255, int(sys.argv[3])): #for multiple iteration
-        thresh = i
-        fn = lambda x : 255 if x > thresh else 0
-        r = img.convert('L').point(fn, mode='1')
-        r.save(dir + '/bg_const_bw_' + str(i) + '.png')
+        bwimg(i, fullname)
+    exit()
 
 else:
     exit()
